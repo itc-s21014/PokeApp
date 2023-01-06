2f42f41c2a8d5c4a8caaf463a4cf7fd135920b2d
s21014 <soccer0907>
2023-01-06 20:56:45 +0900

    世代選択画面と問題画面

--- app/src/main/java/jp/ac/it_college/std/s21014/pokeapp/MainActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s21014/pokeapp/MainActivity.kt
@@ -2,10 +2,30 @@ package jp.ac.it_college.std.s21014.pokeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.addCallback
+import androidx.navigation.findNavController
+import jp.ac.it_college.std.s21014.pokeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        title = getString(R.string.app_name)
+        setContentView(binding.root)
+
+        initJsonData(assets)
+        onBackPressedDispatcher.addCallback(this) {
+            val controller = binding.fragmentContainerView.findNavController()
+            when (controller.currentDestination?.id) {
+                R.id.selectGenerationFragment -> controller.navigate(
+                    SelectGenerationFragmentDirections.selectToTitle()
+                )
+                R.id.quizFragment -> controller.navigate(
+                    QuizFragmentDirections.quizToTitle()
+                )
+            }
+        }
     }
 }
